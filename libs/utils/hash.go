3e096db95179630d2d8a6ee3dec8a25dc1b1f199
Osdare <rex.kengo99@gmail.com>
2025-10-08 19:26:15 +0300

    utils

---
+++ libs/utils/hash.go
@@ -0,0 +1,12 @@
+package utils
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+func HashUrl(normUrl string) string {
+	h := sha256.Sum256([]byte(normUrl))
+	hash := hex.EncodeToString(h[:])
+	return hash
+}
